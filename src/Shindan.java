94f12ff5edc6eeda4516d349cda7f3aa9fefe1ad
Reo Sotoyama <reo.sotoyama.sys@gmail.com>
2017-06-20 10:13:42 +0900

    first commit

---
+++ src/Shindan.java
@@ -0,0 +1,52 @@
+
+public class Shindan {
+
+	public static void main(){
+		//メイン処理をかく
+		int situ1 = situmon1();
+
+		//回答内容で診断結果を考える処理
+
+		//回答結果を出力
+	}
+
+
+
+	public static int situmon1(){
+
+		int answer = 0;
+		//質問内容を表示
+
+		//回答内容を表示
+
+		//ユーザーが答えを入力
+
+		return answer;
+	}
+
+	public static void situmon2(){
+
+
+	}
+
+	public static void situmon3(){
+
+
+	}
+
+	public static void situmon4(){
+
+
+	}
+
+	public static void situmon5(){
+
+
+	}
+
+	public static void situmon6(){
+
+
+	}
+
+}
